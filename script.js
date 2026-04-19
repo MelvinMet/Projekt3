3b0a1305611269955d7c342ede948fb5e6deb371
MelvinMet <Melvin.Metsis@techno.ee>
2026-04-19 20:09:45 +0300

    Valmis

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const joinBTN = document.querySelector("#liitu")
 const inp = document.querySelector("#nimi")
 const maps = document.querySelector("#map")
+const SendMessage = document.querySelector("#saada")
+const inp2 = document.querySelector("#kiri")
+let user = ""
+let playerkey = ""
 
 async function load() {
-      const response = await fetch('https://tinkr.tech/sdb/melvinm/wander');
+  const response = await fetch('https://tinkr.tech/sdb/melvinm/wander');
   const data = await response.json();
-  console.log(data);
+   maps.innerHTML = ""
   for (const idk of data.players) {
     const player = document.createElement("div")
     player.style.position = "absolute"
@@ -15,13 +19,88 @@ async function load() {
     player.style.width = "32px"
     maps.appendChild(player)
 
+    const playerMessage = document.createElement("p")
+    playerMessage.textContent = idk.message + "\n↓"
+    playerMessage.style.textAlign = "center"
+    if (idk.message === null) {
+     playerMessage.textContent = "" 
+    }
+    player.appendChild(playerMessage)
+
     const playerimg = document.createElement("img")
-    playerimg.style.height = "32px"
-    playerimg.style.width = "32px"
-    playerimg.src = "https://tinkr.tech"+ idk.image
-    console.log(idk.image)
+    playerimg.style.height = "40px"
+    playerimg.style.width = "40px"
+    playerimg.src = "https://tinkr.tech" + idk.image
     player.appendChild(playerimg)
+
+    const playername = document.createElement("p")
+    playername.textContent = idk.username
+    player.appendChild(playername)
   }
 }
 
+joinBTN.addEventListener("click", async function () {
+  let username = inp.value
+  const messageData = { action: 'join', username: username }
+
+  const response = await fetch('https://tinkr.tech/sdb/melvinm/wander', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(messageData)
+  });
+
+  const result = await response.json()
+  playerkey = result.player_key
+  user = username
+  load()
+})
+
+maps.addEventListener("click", async function (move) {
+  if (playerkey === "") {
+    alert("Join the game to move")
+  }
+  else {
+    let suurus = maps.getBoundingClientRect()
+    let x = move.clientX - suurus.left
+    let y = move.clientY - suurus.top
+    console.log(x, y)
+
+    const messageData = { action: 'move', player_key: playerkey, x: x, y: y };
+    const response = await fetch('https://tinkr.tech/sdb/melvinm/wander', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(messageData)
+    });
+    load()
+  }
+})
+
+SendMessage.addEventListener("click", async function() {
+  let talk = inp2.value
+    if (playerkey === "" || talk === "") {
+    alert("Something went wrong!")
+  }
+  else {
+  const messageData = { action: 'talk', player_key: playerkey, message: talk };
+        
+  const response = await fetch('https://tinkr.tech/sdb/melvinm/wander', {
+    method: 'POST',
+    headers: {
+    'Content-Type': 'application/json'
+            },
+     body: JSON.stringify(messageData)
+   });
+   inp2.value = ""
+   load()
+  }
+})
+
 load()
+
+setInterval(function() {
+    load();
+}, 2000);
